3b7726853ce259bbe4032178bed083085ef13112
Zack McKenna <zackmckenna@gmail.com>
2020-02-03 17:30:33 -0500

    updated fade in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, {useState, useEffect } from 'react';
 import './App.css';
+import { Box, Grid, Paper } from '@material-ui/core'
+import Fade from './Fade'
+
+import { makeStyles } from '@material-ui/core/styles'
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const [show, setShow] = useState(false)
   const [skill, setSkill] = useState('developer')
   const [activity, setActivity] = useState('is glad you are here')
 
   useEffect(() => {
     randomizeInfo()
   },[])
+
+  const classes = useStyles();
 
   const activities = [
     'loves to bike all over Philly',
@@ -34,7 +51,10 @@ function App() {
   }
 
   const randomizeInfo = () => {
-    debugger
+    setShow(show => !show)
+    setTimeout(() => {
+      setShow(show => !show)
+    }, 4000)
     setTimeout(() => {
       const newSkill = getRandomArrayElement(skills)
       const newActivity = getRandomArrayElement(activities)
@@ -49,9 +69,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>i'm zack mckenna</h1>
-        <h2>a <span style={{ color: '#5bc0de' }}>{skill}</span> who <span style={{ color: '#5cb85c' }}>{activity}</span></h2>
-      </header>
-      <div className='Info'>
+        <Fade show={show}>
+          <h2>a <span style={{ color: '#5bc0de' }}>{skill}</span> who <span style={{ color: '#5cb85c' }}>{activity}</span></h2>
+        </Fade>      </header>
+      <div>
         <h4>Test</h4>
       </div>
     </div>
